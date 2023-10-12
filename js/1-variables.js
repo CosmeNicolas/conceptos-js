3d27883e4be34dab28c39b766f1769ce7430e030
CosmeNicolas <nicomas7590@gmail.com>
2023-10-12 18:33:35 -0300

    agregamos un comentario y una variable en variables.js

---
+++ js/1-variables.js
@@ -0,0 +1,15 @@
+// Esto es un comentario de una única linea
+
+
+/*es es un comentario 
+de 
+multiples lineas */
+
+// declarar una variable (var - let - const)
+/* 
+var: alamcena cosas "datos de manera temporaria"
+let : podemos cambiar su valor 
+const : no deja cambiar el valor 
+*/
+
+let anioActual = 2023;

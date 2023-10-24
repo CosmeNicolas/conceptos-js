815cddde5fef9f22673a41700f6d4402c548b478
CosmeNicolas <nicomas7590@gmail.com>
2023-10-24 18:22:37 -0300

    agregamos un ejemplo de los tres tipos de bucles

---
+++ js/3-bucles.js
@@ -0,0 +1,39 @@
+//while - la condicion logica se tiene q cumplir para q funcione
+/*
+while(condicion logica){
+  aqui va todo el codigo que quiero repetir varias veces
+  agregar un codigo para cambiar la condicion logica 
+}
+*/
+
+
+/*let renglon = 1; 
+
+while(renglon <= 10){
+  document.write(`<p>renglon numero ${renglon}</p>`);
+  renglon++; //renglon = renglon + 1;
+}
+*/
+
+
+//do while - hacer mientras , el codigo se ejecuta al menos una vez 
+/*let renglon = 1; 
+
+do{
+
+  document.write(`<p>renglon numero ${renglon}</p>`);
+  renglon++;
+
+}while(renglon <=10)
+*/
+
+
+//for - si la condicion logica no se cumple , no se ejecuta
+
+
+//for(inicializar una variable ;condicion logica ;incrementar o decrementar una variables)
+
+for(let contador=5; contador > 0 ; contador--){
+  document.write(`<p>Cuenta regresiva ${contador}</p>`)
+}
+
